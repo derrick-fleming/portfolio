b5ba3dd51a4dcc0106bfe19d96ce3d8ba17c296f
Derrick Fleming <derrick.fleming7@gmail.com>
2023-01-06 01:49:31 +0000

    Added text-content and image to home page. Styled some of the content for initial prototyping of page.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 const Home = () => {
   return (
     <Container fluid="lg">
-
+      <Row className="mt-4">
+        <Col sm={5} className="p-4 text-end">
+          <Image fluid roundedCircle src="/images/fleming-photo.jpg" className="profile border border-4 border-secondary shadow-sm"></Image>
+        </Col>
+        <Col sm={7}>
+          <Row className="align-items-center height-100">
+            <Col className='open-sans'>
+              <p className='fs-4 mb-1'>👋 Hi there! I&apos;m</p>
+              <h1 className="oswald mb-2 title-text">Derrick Fleming</h1>
+              <p className='fs-4 mb-0'>Software Developer who loves </p>
+              <p className='fs-4 highlight'>web development. </p>
+              <Button className='me-4'>
+                Projects
+              </Button>
+              <Button>
+                About Me
+              </Button>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     </Container>
   );
 };
